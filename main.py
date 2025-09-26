9932c24f173e06e5f937a59151a51690b9d9c1e9
arteaga7 <cruz.antonio.a.e@gmail.com>
2025-09-25 20:19:52 -0600

    All files made

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
-from datetime import datetime, timedelta
+from datetime import datetime
 import os
 from typing import Tuple    # For python3.8
+from prophet import Prophet
+from sklearn.metrics import mean_absolute_error
+
 
 path_parquet = './data/raw/Lecturas_Eneero_2025'
 path_tables = './data/raw/'
 path_processed = './data/processed'
+FUTURE_DAYS = 6
+RESAMPLE_FREQ = 'H'
+output_dir = './data/processed/prophet_forecasts'
 
 
 def get_data(path: 'str') -> pd.DataFrame:
@@ -82,7 +88,7 @@ def read_tables(path: 'str') -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, 
     return df_cities, df_devices, df_locations, df_sensors, df_sublocations, df_unidades
 
 
-def merge_DataFrames(path_processed, df, df_cities, df_devices, df_locations,
+def merge_DataFrames(path_processed: 'str', df, df_cities, df_devices, df_locations,
                      df_sensors, df_sublocations, df_unidades) -> None:
     """Function to merge DataFrames."""
     df_sensors = df_sensors[['SensorId', 'SensorTyId',
@@ -112,12 +118,117 @@ def merge_DataFrames(path_processed, df, df_cities, df_devices, df_locations,
         path_processed, 'location_subloc_city.csv'), index=False)
 
 
+def preprocessing(df: pd.DataFrame) -> pd.DataFrame:
+    '''Function to preprocess the data'''
+    df['TimeSpan'] = pd.to_datetime(df['TimeSpan'])
+    df['LocalTimeSpan'] = pd.to_datetime(df['TimeSpan'])
+    df = df.drop_duplicates(subset=['ReadId'])
+    df = df[['TimeSpan', 'SensorId', 'Value']]
+    return df
+
+
+def prophet_model(output_dir: 'str', df: pd.DataFrame) -> dict:
+    '''Function to create and train the prophet model for forecasting'''
+    # Convert to compatible format for Prophet (ds, y)
+    df = df.rename(columns={'TimeSpan': 'ds', 'Value': 'y'})
+    df = df.sort_values(by='ds', ascending=True)
+
+    # Resample by hour and obtain the mean of data
+    df_resampled = df.set_index('ds').groupby(
+        'SensorId')['y'].resample(RESAMPLE_FREQ).mean().reset_index()
+
+    # Create directory to save the results
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Obtain all sensors as a list
+    sensor_ids = df_resampled['SensorId'].unique()
+
+    # Dictionary to save the resulting DataFrames
+    results = {}
+
+    for sensor_id in sensor_ids:
+        df_sensor = df_resampled[df_resampled['SensorId'] == sensor_id].copy()
+        df_sensor = df_sensor.dropna(subset=['y'])
+
+        if len(df_sensor) < 2:
+            print(f" ❌ Insufficient data for sensor {sensor_id}. Skipping.")
+            continue
+
+        # Create model. Weekly and daily seasonality is automatically activated
+        # if sufficient data is detected
+        model = Prophet(
+            yearly_seasonality='auto',
+            weekly_seasonality='auto',
+            daily_seasonality='auto',
+            # changepoint_prior_scale=0.03
+        )
+
+        # Fit the model
+        try:
+            model.fit(df_sensor)
+        except Exception as e:
+            print(
+                f" ❌ Error training the model for {sensor_id}: {e}. Skipping.")
+            continue
+
+        # Make forecast
+        future = model.make_future_dataframe(
+            periods=FUTURE_DAYS * 24, freq=RESAMPLE_FREQ)
+        forecast = model.predict(future)
+
+        # Add column 'y' to DataFrame forecast
+        actual_values = df_sensor[['ds', 'y']]
+        forecast = pd.merge(forecast, actual_values, on='ds', how='left')
+
+        # Store the forecast
+        results[sensor_id] = forecast
+
+        # Visualization
+        fig = model.plot(forecast)
+        plt.title(f"Time Series forecast for sensor: {sensor_id}")
+        plt.xlabel("Date")
+        plt.ylabel("Value")
+
+        # Save plot
+        fig.savefig(os.path.join(output_dir, f'forecast_{sensor_id}.png'))
+        plt.close(fig)
+
+    print("\n✅ Process completed.")
+    print(
+        f"The forecast charts have been saved to the directory '{output_dir}'.")
+    print("The results of the predictions are in the 'results' dictionary.")
+    return results
+
+
+def mae(results: dict) -> dict:
+    '''Function to calculate MAE for every model'''
+    mae_scores = {}
+
+    for sensor_id, forecast_df in results.items():
+        historical_comparison = forecast_df.dropna(subset=['y'])
+
+        if len(historical_comparison) == 0:
+            print(
+                f"There are not enough points to calculate the MAE for {sensor_id}.")
+            mae_scores[sensor_id] = np.nan
+            continue
+
+        mae = mean_absolute_error(
+            historical_comparison['y'], historical_comparison['yhat'])
+        mae_scores[sensor_id] = mae
+
+    return mae_scores
+
+
 def main():
     df = get_data(path_parquet)
     df_cities, df_devices, df_locations, df_sensors, df_sublocations, df_unidades = read_tables(
         path_tables)
     merge_DataFrames(path_processed, df, df_cities, df_devices, df_locations,
                      df_sensors, df_sublocations, df_unidades)
+    df = preprocessing(df)
+    results = prophet_model(output_dir, df)
+    mae_scores = mae(results)
 
 
 # Run main function
